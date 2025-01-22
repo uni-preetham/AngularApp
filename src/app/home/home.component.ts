d86f39272af900689510c3d96b4c3b0d3b689452
Preetham A A <preethama@crimsonlogic.com>
2025-01-22 10:57:47 +0530

    last lesson

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -40,7 +40,9 @@ export class HomeComponent {
   housingService: HousingService = inject(HousingService);
 
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
-    this.filteredLocationList = this.housingLocationList;
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
   }
 }
